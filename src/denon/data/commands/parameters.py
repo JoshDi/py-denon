a89cdc212682de99760b5305ba0cc0d4eaf152f7
aaronjames <aaronjames@c203e9e2-fd5e-11dd-ada4-5153b8187bf2>
2009-02-18 19:30:20 +0000

    adding commands/parameter data dictionaries

---
+++ src/denon/data/commands/parameters.py
@@ -0,0 +1,6 @@
+parameters = [
+{'code': 'ON', 'type': 0, 'commands': []},
+{'code': 'STANDBY', 'type': 0, 'commands': []},
+{'code': 'UP', 'type': 0, 'commands': []},
+{'code': 'DOWN', 'type': 0, 'commands': []},
+]
