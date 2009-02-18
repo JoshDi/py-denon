f06c6b64559ed98779906915810375807371bc24
chenry3 <chenry3@c203e9e2-fd5e-11dd-ada4-5153b8187bf2>
2009-02-18 02:20:13 +0000

    added more

--- denon.py
+++ denon.py
@@ -2,11 +2,6 @@
 import serial
 import sys
 from optparse import OptionParser
-try:
-	denSerial = serial.Serial('/dev/tty.usbserial', 9600, timeout=1)
-except serial.serialutil.SerialException:
-	print 'Error cannot open Serial Connection.'
-	sys.exit(0)
 
 def sendCommand( serialConnection, passcommand ):
 	serialConnection.write( passcommand )
@@ -15,12 +10,25 @@ def sendCommand( serialConnection, passcommand ):
 
 p = OptionParser()
 p.add_option("-c", "--command", dest="command", help="Denon Command to use", metavar="COMMAND")
+p.add_option("-p", "--port", dest="port", help="Serial Port dev to use", metavar="PORT")
 (options, args) = p.parse_args()
 
-if len(options.command) > 0:
+if options.port:
+	dev = options.port
+else: 
+	dev = "/dev/tty.usbserial"
+
+try:
+	denSerial = serial.Serial('/dev/tty.usbserial', 9600, timeout=1)
+except serial.serialutil.SerialException:
+	print 'Error cannot open Serial Connection.'
+	sys.exit(0)
+
+if options.command:
 	sendCommand( denSerial, options.command + '\r' )
-else:
+else:	
 	print "Menu of commands soon to come"
+	sys.exit(0)
 
 denSerial.close()
 sys.exit(0)
