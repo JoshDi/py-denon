691683e7714569911739f2deebbdf1e90d6119a7
aaronjames <aaronjames@c203e9e2-fd5e-11dd-ada4-5153b8187bf2>
2009-02-18 19:30:20 +0000

    adding commands/parameter data dictionaries

---
+++ trunk/src/denon/data/commands/commands.py
@@ -0,0 +1,7 @@
+#from denon.data.commands import parameters
+
+commands = [
+{'code': 'PW', 'params': [], 'desc': 'POWERON/STANDBY change'},
+{'code': 'MV', 'params': [], 'desc': 'MASTER VOLUME UP/DOWN, direct change to **dB  **:00 to 99 by ASCII, 80=0dB, 99=--- (MIN)'},
+{'code': 'CV', 'params': [], 'desc': 'CHANNEL VOLUE UP/DOWN, direct change to **dB  ---front Lch  **:38 to 62 by ASCII, 50=0dB'},   
+]
