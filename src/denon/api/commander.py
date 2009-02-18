8a6d6dafbfbc1deb0bb27aece99e0b670ade2634
chenry3 <chenry3@c203e9e2-fd5e-11dd-ada4-5153b8187bf2>
2009-02-18 07:48:46 +0000

    CR is no longer a 'raw' string

--- src/denon/api/commander.py
+++ src/denon/api/commander.py
@@ -7,7 +7,7 @@ class commander(object):
     """
     def __init__(self):
         # @todo: move these settings to a configuration file
-        self.serialPort = '/dev/null'
+        self.serialPort = '/dev/tty.usbserial'
         self.baudRate = 9600
         self.timeout = 1
         self.__conn = None
@@ -16,7 +16,7 @@ class commander(object):
         self.__conn = serial.Serial(self.serialPort, self.baudRate, timeout=self.timeout)
         return self.__conn
 
-    def sendCommand(self, command, cr=r'\r', options={}):
+    def sendCommand(self, command, cr='\r', options={}):
         """ 
             handles passing commands to the serial port.
             @param command: rs232 command to send
