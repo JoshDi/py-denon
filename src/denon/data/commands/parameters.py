ea7715090e26eac98fe226c2786cba823b9d025c
aaronjames <aaronjames@c203e9e2-fd5e-11dd-ada4-5153b8187bf2>
2009-02-20 19:40:11 +0000

    auto configure the python path.  proper error detection on ajax command reqeusts.

--- src/denon/data/commands/parameters.py
+++ src/denon/data/commands/parameters.py
@@ -1,6 +1,6 @@
 parameters = [
-{'code': 'ON', 'type': 0, 'commands': []},
+{'code': 'ON', 'type': 0, 'commands': ['PW']},
 {'code': 'STANDBY', 'type': 0, 'commands': []},
-{'code': 'UP', 'type': 0, 'commands': []},
+{'code': 'UP', 'type': 0, 'commands': ['MV', 'CVFL']},
 {'code': 'DOWN', 'type': 0, 'commands': []},
 ]
